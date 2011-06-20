a5387e36be0f5867e400df6308b083212e2ff6d8
usjusjoken@gmail.com <usjusjoken@gmail.com@39289ad9-63c0-d582-6cb4-fe76935d920a>
2011-06-20 16:44:37 +0000

    Load and Save added from SageTV properties

--- trunk/ADM/src/ADM/MenuItem.java
+++ trunk/ADM/src/ADM/MenuItem.java
@@ -9,7 +9,7 @@ package ADM;
  * @author bir0012
  */
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -21,16 +21,30 @@ import sagex.UIContext;
 
 public class MenuItem {
     public static final String SagePropertyLocation = "ADM/menuitem/";
-    public String Parent = "";
-    public String Name = "";
-    public String ButtonText = "";
-    public String SubMenu = "";
-    public String Action = "";
-    public String ActionType = "";
-    public String BGImageFile = "";
-    public String BGImageFilePath = "";
-    public Boolean IsDefault = false;
+    private String Parent = "";
+    private String Name = "";
+    private String ButtonText = "";
+    private String SubMenu = "";
+    private String Action = "";
+    private String ActionType = "";
+    private String BGImageFile = "";
+    private String BGImageFilePath = "";
+    private Boolean IsDefault = false;
     public static Map<String,MenuItem> MenuItemList = new LinkedHashMap<String,MenuItem>();
+
+    public MenuItem(String bName){
+        //create a MenuItem with just default values
+        Parent = "xTopMenu";
+        Name = bName;
+        ButtonText = "<Not defined>";
+        SubMenu = null;
+        ActionType = null;
+        Action = null;
+        SetBGImageFileandPath(null);
+        IsDefault = false;
+        AddMenuItemtoList(this);
+        
+    }
     
     public MenuItem(String bParent, String bName, String bButtonText, String bSubMenu, String bActionType, String bAction, String bBGImageFile, Boolean bIsDefault){
         Parent = bParent;
@@ -39,24 +53,122 @@ public class MenuItem {
         SubMenu = bSubMenu;
         ActionType = bActionType;
         Action = bAction;
+        SetBGImageFileandPath(bBGImageFile);
+        IsDefault = bIsDefault;
+        AddMenuItemtoList(this);
+        //MenuItemList.put(this.Name, this);
+        
+    }
+
+    public String getAction() {
+        return Action;
+    }
+
+    public void setAction(String Action) {
+        this.Action = Action;
+    }
+
+    public String getActionType() {
+        return ActionType;
+    }
+
+    public void setActionType(String ActionType) {
+        this.ActionType = ActionType;
+    }
+
+    public String getBGImageFile() {
+        return BGImageFile;
+    }
+
+    public void setBGImageFile(String BGImageFile) {
+        SetBGImageFileandPath(BGImageFile);
+    }
+
+//    public String getBGImageFilePath() {
+//        return BGImageFilePath;
+//    }
+//
+//    public void setBGImageFilePath(String BGImageFilePath) {
+//        this.BGImageFilePath = BGImageFilePath;
+//    }
+//
+    public String getButtonText() {
+        return ButtonText;
+    }
+
+    public void setButtonText(String ButtonText) {
+        this.ButtonText = ButtonText;
+    }
+
+    public Boolean getIsDefault() {
+        return IsDefault;
+    }
+
+    public void setIsDefault(Boolean IsDefault) {
+        this.IsDefault = IsDefault;
+    }
+
+    public void setIsDefault(String IsDefault) {
+        if ("true".equals(IsDefault)){
+            this.IsDefault = true;
+        }else{
+            this.IsDefault = false;
+        }
+    }
+
+    public static Map<String, MenuItem> getMenuItemList() {
+        return MenuItemList;
+    }
+
+    public static void setMenuItemList(Map<String, MenuItem> MenuItemList) {
+        MenuItem.MenuItemList = MenuItemList;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getParent() {
+        return Parent;
+    }
+
+    public void setParent(String Parent) {
+        this.Parent = Parent;
+    }
+
+    public String getSubMenu() {
+        return SubMenu;
+    }
+
+    public void setSubMenu(String SubMenu) {
+        this.SubMenu = SubMenu;
+    }
+    
+    final void AddMenuItemtoList(MenuItem NewMenuItem){
+        MenuItemList.put(NewMenuItem.Name, NewMenuItem);
+    }
+    
+    private void SetBGImageFileandPath(String bBGImageFile){
         //see if using a GlobalVariable from a Theme or a path to an image file
-        if (bBGImageFile.contains("\\")){
+        if (bBGImageFile==null){
+            System.out.println("ADM: SetBGImageFileandPath for '" + bBGImageFile + "' - null found");
+            BGImageFile = bBGImageFile;
+            BGImageFilePath = bBGImageFile;
+        }else if (bBGImageFile.contains("\\")){
             //a path to the image file is being used
+            System.out.println("ADM: SetBGImageFileandPath for '" + bBGImageFile + "' - path found");
             BGImageFile = bBGImageFile;
             BGImageFilePath = bBGImageFile;
         }else{
-            //check for null
-            if (bBGImageFile==null){
-                BGImageFile = bBGImageFile;
-                BGImageFilePath = bBGImageFile;
-            }else{
-                //expect a Global Variable from the theme
-                BGImageFile = bBGImageFile;
-                BGImageFilePath = sagex.api.WidgetAPI.EvaluateExpression(new UIContext(sagex.api.Global.GetUIContextName()), bBGImageFile).toString();
-            }
+            //expect a Global Variable from the theme
+            System.out.println("ADM: SetBGImageFileandPath for '" + bBGImageFile + "' - variable found");
+            BGImageFile = bBGImageFile;
+            BGImageFilePath = sagex.api.WidgetAPI.EvaluateExpression(new UIContext(sagex.api.Global.GetUIContextName()), bBGImageFile).toString();
         }
-        IsDefault = bIsDefault;
-        MenuItemList.put(this.Name, this);
         
     }
     
@@ -76,7 +188,7 @@ public class MenuItem {
                 bParentList.add(entry.getValue().Name);
             }
         }         
-        System.out.println("JUSJOKEN: GetMenuItemNameList for '" + Parent + "' :" + bParentList);
+        System.out.println("ADM: GetMenuItemNameList for '" + Parent + "' :" + bParentList);
         
         return bParentList;
     }
@@ -117,27 +229,26 @@ public class MenuItem {
         return MenuItemList.size();
     }
     
-    //saves all MenuItems to Sage properties
-    public static void SaveMenuItemsToSage(){
-        String PropLocation = "";
-        
-        Iterator<Entry<String,MenuItem>> itr = MenuItemList.entrySet().iterator(); 
-        while (itr.hasNext()) {
-            Entry<String,MenuItem> entry = itr.next();
-            PropLocation = SagePropertyLocation + entry.getValue().Name;
-            sagex.api.Configuration.SetProperty(PropLocation + "/Parent", entry.getValue().Action);
-            sagex.api.Configuration.SetProperty(PropLocation + "/ActionType", entry.getValue().ActionType);
-            sagex.api.Configuration.SetProperty(PropLocation + "/BGImageFile", entry.getValue().BGImageFile);
-            sagex.api.Configuration.SetProperty(PropLocation + "/ButtonText", entry.getValue().ButtonText);
-            sagex.api.Configuration.SetProperty(PropLocation + "/Name", entry.getValue().Name);
-            sagex.api.Configuration.SetProperty(PropLocation + "/Parent", entry.getValue().Parent);
-            sagex.api.Configuration.SetProperty(PropLocation + "/SubMenu", entry.getValue().SubMenu);
-            sagex.api.Configuration.SetProperty(PropLocation + "/IsDefault", entry.getValue().IsDefault.toString());
-        }         
-        System.out.println("JUSJOKEN: SaveMenuItemsToSage: saved " + MenuItemList.size() + " MenuItems");
-        
-        return;
-    }
-    
+//    //saves all MenuItems to Sage properties
+//    public static void SaveMenuItemsToSage(){
+//        String PropLocation = "";
+//        
+//        Iterator<Entry<String,MenuItem>> itr = MenuItemList.entrySet().iterator(); 
+//        while (itr.hasNext()) {
+//            Entry<String,MenuItem> entry = itr.next();
+//            PropLocation = SagePropertyLocation + entry.getValue().Name;
+//            sagex.api.Configuration.SetProperty(PropLocation + "/Parent", entry.getValue().Action);
+//            sagex.api.Configuration.SetProperty(PropLocation + "/ActionType", entry.getValue().ActionType);
+//            sagex.api.Configuration.SetProperty(PropLocation + "/BGImageFile", entry.getValue().BGImageFile);
+//            sagex.api.Configuration.SetProperty(PropLocation + "/ButtonText", entry.getValue().ButtonText);
+//            sagex.api.Configuration.SetProperty(PropLocation + "/Name", entry.getValue().Name);
+//            sagex.api.Configuration.SetProperty(PropLocation + "/Parent", entry.getValue().Parent);
+//            sagex.api.Configuration.SetProperty(PropLocation + "/SubMenu", entry.getValue().SubMenu);
+//            sagex.api.Configuration.SetProperty(PropLocation + "/IsDefault", entry.getValue().IsDefault.toString());
+//        }         
+//        System.out.println("ADM: SaveMenuItemsToSage: saved " + MenuItemList.size() + " MenuItems");
+//        
+//        return;
+//    }
     
 }
