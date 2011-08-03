6c6dc687f25108ead15f407c4072f29c4f6c550a
usjusjoken <usjusjoken@gmail.com>
2011-08-03 22:06:43 +0000

    Security Module complete - version 0.401

--- ADM/src/ADM/util.java
+++ ADM/src/ADM/util.java
@@ -24,7 +24,7 @@ import java.util.TreeMap;
 
 public class util {
 
-    public static String Version = "0.400";
+    public static String Version = "0.401";
     public static final String ListToken = ":&&:";
     public static final String PropertyComment = "---ADM MenuItem Properties - Do Not Manually Edit---";
     public static final String PropertyBackupFile = "ADMbackup.properties";
