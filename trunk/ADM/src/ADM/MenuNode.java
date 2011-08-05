30e984f895d161ca80aaeec136f42d32ad4c9ece
usjusjoken@gmail.com <usjusjoken@gmail.com@39289ad9-63c0-d582-6cb4-fe76935d920a>
2011-08-05 13:46:02 +0000

    Quick Launch Menu QLM started -  version 0.410

--- trunk/ADM/src/ADM/MenuNode.java
+++ trunk/ADM/src/ADM/MenuNode.java
@@ -650,6 +650,18 @@ public class MenuNode {
         }
     }
 
+    //special case so SageSubMenus do not get displayed
+    public static String GetMenuItemSubMenuQLM(String Name){
+        try {
+            return MenuNodeList().get(Name).getSubMenuExcludingSageMenus();
+        } catch (Exception e) {
+            System.out.println("ADM: mGet... ERROR: Value not available for '" + Name + "' Exception = '" + e + "'");
+            return null;
+        }
+    }
+
+    //TODO: need function to get menu item list EXCLUDING items with a Sage Submenu and no default action
+
     public static String GetMenuItemSubMenu(String Name){
         try {
             return MenuNodeList().get(Name).getSubMenu();
