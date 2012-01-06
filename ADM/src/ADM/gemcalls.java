0a7ce4afdad857306197866b2be1120b848adb25
usjusjoken <usjusjoken@gmail.com>
2012-01-06 21:24:02 +0000

    ADM updates to work with gemstone - version 0.430

---
+++ ADM/src/ADM/gemcalls.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ADM;
+
+import java.util.ArrayList;
+import sagex.UIContext;
+import Diamond.Flow;
+
+/**
+ *
+ * @author jusjoken
+ */
+public class gemcalls {
+    
+    public static Boolean Isgemstone(){
+        String gemstoneWidgetSymbol = "JUSJOKEN-3084835";
+        // check to see if the gemstone Plugin is installed
+        Object[] FoundWidget = new Object[1];
+        FoundWidget[0] = sagex.api.WidgetAPI.FindWidgetBySymbol(new UIContext(sagex.api.Global.GetUIContextName()), gemstoneWidgetSymbol);
+        if (FoundWidget[0]!=null){
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+
+    public static ArrayList<String> GetFlows(){
+        ArrayList<String> tList = new ArrayList<String>();
+        try {
+            tList = Flow.GetFlows();
+        } catch (NoClassDefFoundError e) {
+            System.out.println("ADM gemcalls : gemstone class not found for Flow.GetFlows");
+        }
+        return tList;
+    }
+    
+}
